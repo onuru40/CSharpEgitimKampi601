4b63685c489e46928a859bca76cd7bff4949c318
onuru40 <onur.unlu40@gmail.com>
2025-02-02 02:18:08 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,73 @@
+﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text)
+            };
+
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Müşteri ekleme işlemi başarılı","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void btnCustomerList_Click(object sender, EventArgs e)
+        {
+            List<Customer> customers = customerOperations.GetAllCustomer();
+            dataGridView1.DataSource = customers;
+        }
+
+        private void btnCustomerDelete_Click(object sender, EventArgs e)
+        {
+            string customerId = txtCustomerId.Text;
+            customerOperations.DeleteCustomer(customerId);
+            MessageBox.Show("Müşteri başarıyla silindi");
+        }
+
+        private void btnCustomerUpdate_Click(object sender, EventArgs e)
+        {
+            string id = txtCustomerId.Text;
+            var updateCustomer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text),
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerId = id
+            };
+            customerOperations.UpdateCustomer(updateCustomer);
+            MessageBox.Show("Müşteri başarıyla güncellendi");
+        }
+
+        private void btnGetByCustomerId_Click(object sender, EventArgs e)
+        {
+            string id = txtCustomerId.Text;
+            Customer customers = customerOperations.GetCustomerById(id);
+            dataGridView1.DataSource = new List<Customer> { customers };
+        }
+    }
+}
